bc6094d3f1b7918ca0d15118edadecd28d1fb0d5
James Peres <james.peres.s@gmail.com>
2017-04-18 20:02:21 -0300

    add bootstrap

--- telegramBot/telegramBot/settings.py
+++ telegramBot/telegramBot/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'telegrambot',
+    'rest_framework',
+    'bot',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
