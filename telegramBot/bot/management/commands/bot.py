efe407a3592127100f5e5638569e5786f5fde748
James Peres <james.peres.s@gmail.com>
2017-04-24 02:27:57 -0300

    update bot start

--- telegramBot/bot/management/commands/bot.py
+++ telegramBot/bot/management/commands/bot.py
@@ -10,20 +10,11 @@ class Command(BaseCommand):
         update.message.reply_text('Seu código de acesso é {}'.format(update.message.from_user.id))
         update.message.reply_text('Favor Cadastrar em nosso sistema ')
 
-    def hello(bot, update):
-        update.message.reply_text(
-            'Hello {}'.format(update.message.from_user.first_name))
 
-
-    def hora(bot, update):
-        update.message.reply_text(
-            'horas {}:{} : {}'.format(now.hour, now.minute, now))
-
-    updater = Updater('228843118:AAGk6hkBpjIW_DazSEv843WwD_SMCuOFS0M')
+    updater = Updater('-------TOKEN AQUI---------')
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('hello', hello))
-    updater.dispatcher.add_handler(CommandHandler('hora', hora))
+
 
     updater.start_polling()
     updater.idle()
