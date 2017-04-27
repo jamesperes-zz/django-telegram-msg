1181719640ed53ee8f10282eeddc1415a62c553a
James Peres <james.peres.s@gmail.com>
2017-04-27 12:53:36 -0300

    tratando erros

--- telegramBot/bot/management/commands/bot.py
+++ telegramBot/bot/management/commands/bot.py
@@ -9,10 +9,23 @@ from django.core.files import File
 import tempfile
 
 now = datetime.now()
+tmp = tempfile.gettempdir()
+
+
+def upload(usuario, telegramfile, model, modelfield):
+    nome = basename(telegramfile.file_path)
+    caminho = os.path.join(tmp, nome)
+    telegramfile.download(custom_path=caminho)
+    obj = model(usuario=usuario)
+    getattr(obj, modelfield).save(
+        os.path.basename(caminho),
+        File(open(caminho, 'rb')))
+    obj.save()
+    os.remove(caminho)
 
 
 class Command(BaseCommand):
-    updater = Updater('TOKEN AQUI')
+    updater = Updater('228843118:AAGk6hkBpjIW_DazSEv843WwD_SMCuOFS0M')
 
     def start(bot, update):
         update.message.reply_text('Ola {}, bem vindo ao nosso Bot de comunicação'.format(
@@ -25,19 +38,13 @@ class Command(BaseCommand):
         nomes = UsuarioTelegram.objects.all()
         user = update.message.from_user.first_name
         photo_file = bot.getFile(update.message.photo[-1].file_id)
-        tmp = tempfile.gettempdir()
 
         for a in nomes:
             if a.nome == user:
-                nome = basename(photo_file.file_path)
-                caminho = os.path.join(tmp, nome)
-                photo_file.download(custom_path=caminho)
-                obj = Imagem(usuario=a)
-                obj.imagem.save(
-                    os.path.basename(caminho),
-                    File(open(caminho, 'rb')))
-                obj.save()
-                os.remove(caminho)
+                try:
+                    upload(a, photo_file, Imagem, 'imagem')
+                except:
+                    print('Falha de upload')
         print('imagem enviada')
         update.message.reply_text('Foto enviada para o sistema ')
 
@@ -47,8 +54,10 @@ class Command(BaseCommand):
         doc_file = bot.getFile(update.message.document.file_id)
         for a in nomes:
             if a.nome == user:
-                Documento.objects.create(
-                    usuario=a, documento=doc_file.download())
+                try:
+                    upload(a, doc_file, Documento, 'documento')
+                except:
+                    print('Falha no upload')
         print('Doc enviado')
         update.message.reply_text('Documento enviado para o sistema ')
 
